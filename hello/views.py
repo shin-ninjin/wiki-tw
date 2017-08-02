b043cd5834720f4bdfb9c57f9f9dfeb5dd1e67ee
shin-ninjin <derodero317@gmail.com>
2017-08-02 21:49:10 +0900

    Demo

--- hello/views.py
+++ hello/views.py
@@ -8,27 +8,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-	CONSUMER_KEY = os.environ.get('CONSUMER_KEY',3)
-	CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET',3)
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN',3)
-	ACCESS_SECRET = os.environ.get('ACCESS_SECRET',3)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-
-	#APIインスタンスを作成
-	api = tweepy.API(auth)
-
-	# これだけで、Twitter APIをPythonから操作するための準備は完了。
-
-	url ='https://ja.wikipedia.org/w/api.php?format=json&utf8&action=query&list=random&rnnamespace=0&rnlimit=1'
-
-	r=urllib.request.urlopen(url)
-	root=json.loads(r.read())
-
-	st = 'title:' + root['query']['random'][0]['title']
-	print(st)
-	api.update_status(status=st)
-	return HttpResponse('Hello! ' + st)
+	return HttpResponse('Hello! ')
 
 
 def db(request):
