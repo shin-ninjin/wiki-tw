f0931c33a20d27290911174ddc5d072477f1af28
shin-ninjin <derodero317@gmail.com>
2017-08-02 21:10:05 +0900

    Demo

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
